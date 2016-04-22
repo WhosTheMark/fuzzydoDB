94ecb0481cc40fd8860dbedc72176f006fa62a17
Marcos Campos <whosthemark@gmail.com>
2016-04-22 16:05:37 -0430

    Added AngularJS to the project. Changed to jQuery2

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+angular.module('root', [])
+    .controller("index", ["$scope", function ($scope) {
+        $scope.message = "Hello World in AngularJS :D !";
+    }]);
