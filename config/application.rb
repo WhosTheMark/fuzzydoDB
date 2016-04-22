b44b964d1cdda154621a27dbde7a0f550671fd39
Marcos Campos <whosthemark@gmail.com>
2016-04-22 18:14:06 -0430

    Integrated TypeScript into the project

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module FuzzydoDBapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    Typescript::Rails::Compiler.default_options = %w(--target ES6)
   end
 end
