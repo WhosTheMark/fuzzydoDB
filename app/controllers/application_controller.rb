6fd83c13b5652ec150188e59005a9bb7647fa92f
John <pexison@gmail.com>
2016-04-24 12:45:10 -0430

    we need angularjs-rails integration for translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,34 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+   
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+  
+  # Get locale from top-level domain or return nil if such locale is not available
+  # # You have to put something like:
+  # #   127.0.0.1 application.com
+  # #   127.0.0.1 application.it
+  # #   127.0.0.1 application.pl
+  # # in your /etc/hosts file to try this out locally
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+  
+  # Get locale code from request subdomain (like http://it.application.local:3000)
+  # You have to put something like:
+  #   127.0.0.1 gr.application.local
+  # in your /etc/hosts file to try this out locally
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+      I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+  
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 end
