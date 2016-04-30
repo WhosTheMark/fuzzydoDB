35ac1abc17e94c11bf9b69f1beb0d60df4295900
Marcos Campos <whosthemark@gmail.com>
2016-04-30 00:32:42 -0430

    Defined custom url helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
-   
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-  
+
   # Get locale from top-level domain or return nil if such locale is not available
   # # You have to put something like:
   # #   127.0.0.1 application.com
@@ -18,5 +18,9 @@ class ApplicationController < ActionController::Base
   def extract_locale_from_tld
     parsed_locale = request.host.split('.').last
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def default_url_options(options={})
+    { :locale => I18n.locale }
   end
 end
