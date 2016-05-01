2ada527f069c701d445b69f7ea75ba908d89001a
John <pexison@gmail.com>
2016-05-01 15:59:27 -0430

    about us page transitions were added

--- app/assets/javascripts/about/about.ts
+++ app/assets/javascripts/about/about.ts
@@ -1,2 +1,21 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+/// <reference path="../typings/angular.d.ts" />
+
+function teamHideShowSections(elem) {
+  var selected_section_id = elem.id.split("_")[0];
+  var selected_section = document.getElementById(selected_section_id);
+
+
+  if (!selected_section.classList.contains("section-active")) {
+    var sections = document.getElementsByClassName("about-us-sections");
+    var i;
+  
+    for (i = 0; i < sections.length; i++) {
+       $('#' + sections[i].id).fadeOut(10);
+       document.getElementById(sections[i].id).classList.remove("section-active");
+    }
+  
+    selected_section.classList.add("section-active");
+    setTimeout(function() {}, 200);
+    $('#' + selected_section_id).fadeIn(500);
+  }
+}
