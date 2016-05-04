6510b07ab49ae64140644094dec544dc9db470ec
John <pexison@gmail.com>
2016-05-04 00:02:53 -0430

    go to top btn created

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,21 @@ $(window).scroll(function() {
     $('.header-x-scroll').css('left', -$(this).scrollLeft() + "px");
     $('.content-left').css('left', -$(this).scrollLeft() + "px");
 });
+
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.crunchify-top').fadeIn(duration);
+        } else {
+            jQuery('.crunchify-top').fadeOut(duration);
+        }
+    });
+
+    jQuery('.crunchify-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
