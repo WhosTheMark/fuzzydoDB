78c1e74205b7895d4bc4553e1640b77c51df21e1
John <pexison@gmail.com>
2016-05-04 00:08:15 -0430

    default image for team members changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,40 +4,32 @@ I18n.locale = :es
 
 Member.create!(member_id: "rodriguez",
                name: "Rosseline Rodríguez",
-               photo: "rosseline.png",
-               bio: "erygdfgdfgdfgdfg")
+               photo: "rosseline.png")
 
 Member.create!(member_id: "tineo",
                name: "Leonid Tineo",
-               photo: "darwin.png",
-               bio: "")
+               photo: "default.png")
 
 Member.create!(member_id: "carrasquel",
                name: "Soraya Carrasquel",
-               photo: "darwin.png",
-               bio: "")
+               photo: "default.png")
 
 Member.create!(member_id: "coronado",
                name: "David Coronado",
-               photo: "darwin.png",
-               bio: "")
+               photo: "default.png")
 
 Member.create!(member_id: "cadenas",
                name: "José Cadenas",
-               photo: "darwin.png",
-               bio: "")
+               photo: "default.png")
 
 Member.create!(member_id: "ramirez",
                name: "Josué Ramírez",
-               photo: "darwin.png",
-               bio: "")
+               photo: "default.png")
 
 Member.create!(member_id: "monascal",
                name: "Ricardo Monascal",
-               photo: "darwin.png",
-               bio: "")
+               photo: "default.png")
 
 Member.create!(member_id: "rocha",
                name: "Darwin Rocha",
-               photo: "darwin.png",
-               bio: "")
+               photo: "darwin.png")
