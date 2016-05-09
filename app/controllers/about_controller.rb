6400d501017b171e38a686c711fbb999fa697029
Marcos Campos <whosthemark@gmail.com>
2016-05-08 22:02:30 -0400

    Added missing translation. Refactoring

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,16 +1,16 @@
 class AboutController < ApplicationController
 
   def index
-    @title = "about-us-introduction.title"
-    @content = "about-us-introduction.content"
+    @title = "about-us.introduction.title"
+    @content = "about-us.introduction.content"
     @active_item = "introduction"
 
     render "about_us_plain"
   end
 
   def history
-    @title = "about-us-history.title"
-    @content = "about-us-history.content"
+    @title = "about-us.history.title"
+    @content = "about-us.history.content"
     @active_item = "history"
 
     render "about_us_plain"
