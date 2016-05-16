9ff91dc9970bd49927ec2199dac723b05273f487
Marcos Campos <whosthemark@gmail.com>
2016-05-16 02:05:42 -0400

    Prevented form submit on login/registration if there are any errors

--- app/assets/javascripts/users/UserController.ts
+++ app/assets/javascripts/users/UserController.ts
@@ -2,5 +2,26 @@
 
 angular.module("fuzzydodb.user", [])
   .controller("UserController", ["$scope", function($scope) {
+
     $scope.loginUser = {};
+    $scope.registerUser = {};
+
+    $scope.submitLogin = function($event) {
+
+      $scope.loginForm.$submitted = true;
+
+      if ($scope.loginForm.$invalid) {
+        $event.preventDefault();
+      };
+    };
+
+    $scope.submitRegistration = function($event) {
+
+      $scope.registrationForm.$submitted = true;
+
+      if ($scope.registrationForm.$invalid) {
+        $event.preventDefault();
+      };
+    };
+
   }]);
