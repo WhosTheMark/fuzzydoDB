8150be8fe9435f6458675f01c4cfc7ea1dc91807
Marcos Campos <whosthemark@gmail.com>
2016-05-20 01:49:30 -0400

    Simplified session controller :)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,5 @@
 class SessionsController < Devise::SessionsController
 
-  # Override create method
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-
-    respond_to do |format|
-      p format
-      format.html { respond_with resource, location: after_sign_in_path_for(resource) }
-      format.json { render :json  => {}, :status => :ok }
-    end
-  end
+  respond_to :json
 
 end
