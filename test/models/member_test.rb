36218f72f7c453a9086293ea5864806761f68e54
John <pexison@gmail.com>
2016-05-21 23:51:54 -0400

    real tests added

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -5,56 +5,57 @@ Mongoid.raise_not_found_error = false
 
 class MemberTest < ActiveSupport::TestCase
   test "create member without member_id" do
-     member = Member.create(member_id: "", name: "Raid", email: "raid@usb.ve", photo: "default.png")
-     assert !(member.save)
+    member = Member.create(member_id: "", name: "Raid", email: "raid@usb.ve", photo: "default.png")
+    assert !(member.save)
   end
 
   test "create member without name" do
-     member = Member.create(member_id: "raid", name: "", email: "raid@usb.ve", photo: "default.png")
-     assert !(member.save)
+    member = Member.create(member_id: "raid", name: "", email: "raid@usb.ve", photo: "default.png")
+    assert !(member.save)
   end
 
   test "create member without email" do
-     member = Member.create(member_id: "raid", name: "Raid", email: "", photo: "default.png")
-     assert !(member.save)
+    member = Member.create(member_id: "raid", name: "Raid", email: "", photo: "default.png")
+    assert !(member.save)
   end
 
   test "create member without photo" do
-     member = Member.create(member_id: "raid", name: "Raid", email: "raid@usb.ve", photo: "")
-     created = member.save
-     Member.where(member_id: "raid").delete
-     deleted = !Member.where(member_id: "raid").exists?
-     assert created && deleted
+    created = Member.create(member_id: "raid123456", name: "Raid", email: "raid123456@usb.ve", photo: "").save
+    Member.where(member_id: "raid123456").delete
+    assert created
   end
 
-  test "create member 1" do
-     member = Member.create(member_id: "delgado", name: "John Delgado", email: "10-10196@usb.ve", photo: "default.png")
-     assert !(member.save)
+  test "create duplicate user" do
+    Member.create(member_id: "raid", name: "Raid", email: "raid@usb.ve", photo: "").save
+    saved = Member.create(member_id: "raid", name: "Raid", email: "raid@usb.ve", photo: "").save
+    User.where(member_id: "raid").delete
+    assert !(saved)
   end
 
-   test "create member 2" do
-     existed = !(Member.where(member_id: "camomila").exists?)
-     Member.create(member_id: "camomila", name: "camomila", email: "pe@gmail.com")
-     created = Member.find_by(member_id: "camomila").save
-     Member.where(member_id: "camomila").delete
-     deleted = !(Member.where(member_id: "camomila").exists?)
-     assert existed && created && deleted
-   end
+  test "get user that exists" do
+    Member.create(member_id: "delgado", name: "John Delgado", email: "10-10196@usb.ve").save
+    member = Member.get_by_id "delgado"
+    Member.where(member_id: "delgado").delete
+    assert member
+  end
 
-   test "get member john" do
-     assert Member.where(name: "John Delgado")
-   end
+  test "get user that doesnt exist" do
+    assert !Member.get_by_id("delgado")
+  end
 
-   test "get member marcos" do
-     assert Member.find_by(member_id: "campos")
-   end
+  test "get non developers 1 devop 1 member" do
+    Developer.create(member_id: "delgadoD1", name: "John Delgado", email: "10-10196@usb.ve").save
+    Member.create(member_id: "delgado1", name: "John Delgado", email: "1010196@usb.ve").save
+    members = Member.get_non_developers
+    added = members.first[:member_id].eql? "delgado1"
+    Member.where(member_id: "delgado1").delete
+    Developer.where(member_id: "delgadoD1").delete
+    assert added
+  end
 
-   test "get member poogle" do
-     assert !Member.find_by(member_id: "poogle")
-   end
-
-   test "delete member poogle" do
-     assert 0.0 == Member.where(member_id: "poogle").delete
-   end
+  test "get non developers 1 devop 0 member" do
+    Developer.create(member_id: "delgadoD", name: "John Delgado", email: "10-10196@usb.ve").save
+    Developer.where(member_id: "delgadoD").delete
+    assert Member.get_non_developers.first.nil?
+  end
 end
-
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,47 +5,59 @@ Mongoid.raise_not_found_error = false
 
 class UserTest < ActiveSupport::TestCase
   test "create user without username" do
-     user = User.create(username: "", name: "Raid", email: "raid@usb.ve")
-     assert !(user.save)
+    user = User.create(username: "", name: "Raid", email: "raid@usb.ve", password: "12313223sasdfawsdaw")
+    assert !(user.save)
   end
 
   test "create user without name" do
-     user = User.create(username: "raid", name: "", email: "raid@usb.ve")
-     assert !(user.save)
+    user = User.create(username: "raid", name: "", email: "raid@usb.ve", password: "12313223sasdfawsdaw")
+    assert !(user.save)
   end
 
   test "create user without email" do
-     user = User.create(username: "raid", name: "Raid", email: "")
-     assert !(user.save)
+    user = User.create(username: "raid", name: "Raid", email: "", password: "12313223sasdfawsdaw")
+    assert !(user.save)
   end
 
-  test "create user 1" do
-     user = User.create(username: "delgado", name: "John Delgado", email: "10-10196@usb.ve")
-     assert !(user.save)
+  test "email exists" do
+    User.create(username: "johndelgado", name: "John Delgado", email: "1010196@usb.ve", password: "12313223sasdfawsdaw").save
+    exists = User.exists_email? "1010196@usb.ve"
+    User.where(username: "johndelgado").delete
+    assert exists
   end
 
-   test "create user 2" do
-     existed = !(User.where(username: "camomila").exists?)
-     User.create(username: "camomila", name: "camomila", email: "pe@gmail.com", password: "12313223sasdfawsdaw")
-     created = User.find_by(username: "camomila").save
-     User.where(username: "camomila").delete
-     deleted = !(User.where(username: "camomila").exists?)
-     assert existed && created && deleted
-   end
+  test "email does not exists" do
+    exists = User.exists_username? "10-10196@usb.ve"
+    assert !(exists)
+  end
 
-   test "get user john" do
-     assert User.where(name: "John Delgado")
-   end
+  test "username exists" do
+    User.create(username: "johndelgado", name: "John Delgado", email: "1010196@usb.ve", password: "12313223sasdfawsdaw").save
+    exists = User.exists_username? "johndelgado"
+    User.where(username: "johndelgado").delete
+    assert exists
+  end
 
-   test "get user marcos" do
-     assert User.find_by(username: "whosthemark")
-   end
+  test "username does not exists" do
+    exists = User.exists_email? "1010196"
+    assert !(exists)
+  end
 
-   test "get user poogle" do
-     assert !User.find_by(username: "poogle")
-   end
+  test "create duplicate user" do
+    User.create(username: "delgado", name: "John Delgado", email: "10-10196@usb.ve", password: "12313223sasdfawsdaw").save
+    saved = User.create(username: "delgado", name: "John Delgado", email: "10-10196@usb.ve", password: "12313223sasdfawsdaw").save
+    User.where(username: "delgado").delete
+    assert !(saved)
+  end
 
-   test "delete user poogle" do
-     assert 0.0 == User.where(username: "poogle").delete
-   end
+  test "get user that exists" do
+    User.create(username: "delgado", name: "John Delgado", email: "10-10196@usb.ve").save
+    found = User.where(name: "John Delgado")
+    User.where(username: "delgado").delete
+    assert found
+  end
+
+  test "get user that doesnt exist" do
+    assert !User.find_by(username: "whosthemark")
+  end
 end
