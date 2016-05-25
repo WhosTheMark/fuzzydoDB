46a39202c5a2489213f4efe5f7798eca2060227e
John <pexison@gmail.com>
2016-05-25 15:58:14 -0400

    default.png photo for members added as member model before validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -20,11 +20,15 @@ class Member
     self.email = self.email.strip unless self.email.nil?
   end
 
+  def set_default_img
+    self.photo = "default.png" if self.photo.nil? || (self.photo.size > 0) 
+  end
 
   validates :member_id, presence: true, uniqueness: true
   validates :name     , presence: true
   validates :email    , presence: true, uniqueness: true, format: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
 
   before_validation :strip_whitespace, :only => [:member_id, :email]
+  before_validation :set_default_img, :only => [:photo]
 
 end
