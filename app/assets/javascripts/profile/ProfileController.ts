c0c1a544ca7eb0059f7dd4441d9425a5f252701f
Marcos Campos <whosthemark@gmail.com>
2016-06-05 22:25:09 -0400

    Fixed issued with angular profile module

--- app/assets/javascripts/profile/ProfileController.ts
+++ app/assets/javascripts/profile/ProfileController.ts
@@ -12,3 +12,31 @@ angular.module("fuzzydodb.profile", [])
 
   }])
 
+  // example Fiddle: https://jsfiddle.net/alexk111/rw6q9/
+  .controller("ProfilePhotoController", ["$scope",
+    function($scope) {
+
+      $scope.myImage = '';
+      $scope.myCroppedImage = '';
+
+      var handleFileSelect = function(evt: Event) {
+
+        var currentTarget: any = evt.currentTarget;
+        var file: Blob = currentTarget.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(evt) {
+
+          $scope.$apply(function($scope) {
+
+            var target: any = evt.target;
+            $scope.myImage = target.result;
+          });
+        };
+
+        reader.readAsDataURL(file);
+      };
+
+      angular.element(document.querySelector('#fileInput')).on('change', handleFileSelect);
+  }]);
+
